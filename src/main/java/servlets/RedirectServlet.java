1bf65c461cdeba37c7d33d13980d9c18dbfdf091
Roman <romanfirs@list.ru>
2023-01-24 10:58:21 +0800

    .

---
+++ src/main/java/servlets/RedirectServlet.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        System.out.println("init() сработал!");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("сработал гет");
+
+        resp.sendRedirect("https://www.google.com");
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("destroy() Сработал!");
+    }
+}
